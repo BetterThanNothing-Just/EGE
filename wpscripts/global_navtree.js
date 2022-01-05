7927acd06616f4a7fd7a6cc3a07a526ae382a786
BetterThanNothing-Just <97177968+BetterThanNothing-Just@users.noreply.github.com>
2022-01-05 17:16:54 +0000

    Add files via upload
    Archive of Eddi Goes east website supporting Eddi Piper's project in Sri Lanka 2010-2014

---
+++ wpscripts/global_navtree.js
@@ -0,0 +1,43 @@
+var global_navtree = WpNavBar.readTree({
+"childArray" : [
+{   'sTitle':'Home',
+    'bIsWebPath':true,
+    'sUrl':'index.html'
+},
+{   'sTitle':'Guest Room',
+    'bIsWebPath':true,
+    'sUrl':'guestroom.html'
+},
+{   'sTitle':'The Project',
+    'bIsWebPath':true,
+    'sUrl':'the_project.html'
+},
+{   'sTitle':'Videos',
+    'bIsWebPath':true,
+    'sUrl':'videos.html'
+},
+{   'sTitle':'Map',
+    'bIsWebPath':true,
+    'sUrl':'map.html'
+},
+{   'sTitle':'Fund Raising',
+    'bIsWebPath':true,
+    'sUrl':'fund_raising.html'
+},
+{   'sTitle':'Shiny Cushion',
+    'bIsWebPath':true,
+    'sUrl':'shiny_cushion.html'
+},
+{   'sTitle':'Weblog',
+    'bIsWebPath':true,
+    'sUrl':'weblog.html'
+},
+{   'sTitle':'Mailing List',
+    'bIsWebPath':true,
+    'sUrl':'mailing_list.html'
+},
+{   'sTitle':'Contact & Links',
+    'bIsWebPath':true,
+    'sUrl':'contact_and_links.html'
+}]
+});
